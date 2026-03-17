177fa767a2c60e4e3a8ca1a8ed99a8bd70cb3a88
NhatBarbie001 <LDnhat001@gmail.com>
2026-03-18 00:42:43 +0700

    ..

--- loraclip/loralib/layers.py
+++ loraclip/loralib/layers.py
@@ -880,10 +880,10 @@ def multi_head_attention_forward(query: Tensor,
                 v = linear(value, v_proj_weight_non_opt, in_proj_bias)
 
             # q += linear(linear(query, q_proj_weight_non_opt_A), q_proj_weight_non_opt_B) * q_proj_weight_scaling
-            k += linear(linear(key, k_proj_weight_non_opt_A), k_proj_weight_non_opt_B) * k_proj_weight_scaling
-            v += linear(linear(value, v_proj_weight_non_opt_A), v_proj_weight_non_opt_B) * v_proj_weight_scaling
-            # k += linear(key, delta_w_k) * k_proj_weight_scaling
-            # v += linear(value, delta_w_v) * v_proj_weight_scaling
+            # k += linear(linear(key, k_proj_weight_non_opt_A), k_proj_weight_non_opt_B) * k_proj_weight_scaling
+            # v += linear(linear(value, v_proj_weight_non_opt_A), v_proj_weight_non_opt_B) * v_proj_weight_scaling
+            k += linear(key, delta_w_k) * k_proj_weight_scaling
+            v += linear(value, delta_w_v) * v_proj_weight_scaling
 
             pass
         elif mlp:
