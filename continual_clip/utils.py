8e0e515a8c3795251648c02b480ebd9a5432382e
NhatBarbie001 <LDnhat001@gmail.com>
2026-03-17 22:03:14 +0700

    ...

--- continual_clip/utils.py
+++ continual_clip/utils.py
@@ -36,7 +36,7 @@ def save_config(config: DictConfig) -> None:
 
 def get_workdir(path):
     split_path = path.split("/")
-    workdir_idx = split_path.index("MindtheGap")
+    workdir_idx = split_path.index("Mind_FFT_Gap_v2")
     return "/".join(split_path[:workdir_idx+1])
 
 
