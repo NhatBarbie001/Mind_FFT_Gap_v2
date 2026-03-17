f53c3a94ac71f19c8630019825f9ca716fa7c1e0
NhatBarbie001 <LDnhat001@gmail.com>
2026-03-18 01:47:13 +0700

    ...

--- main.py
+++ main.py
@@ -115,19 +115,21 @@ def run_class_incremental(cfg, device):
     for name, param in model.named_parameters():
             param.requires_grad_(False)
             try:
-                if "classifier_pool" + "." + str(model.module.numtask - 1) in name:
-                    param.requires_grad_(True)
-                if "coef_k" + "." + str(model.module.numtask - 1) in name:
-                    param.requires_grad_(True)
-                if "coef_v" + "." + str(model.module.numtask - 1) in name:
-                    param.requires_grad_(True)
+                for task_id in range(cfg.task_num):
+                    if "classifier_pool" + "." + str(task_id) in name:
+                        param.requires_grad_(True)
+                    if "coef_k" + "." + str(task_id) in name:
+                        param.requires_grad_(True)
+                    if "coef_v" + "." + str(task_id) in name:
+                        param.requires_grad_(True)
             except:
-                if "classifier_pool" + "." + str(model.numtask - 1) in name:
-                    param.requires_grad_(True)
-                if "coef_k" + "." + str(model.numtask - 1) in name:
-                    param.requires_grad_(True)
-                if "coef_v" + "." + str(model.numtask - 1) in name:
-                    param.requires_grad_(True)
+                for task_id in range(cfg.task_num):
+                    if "classifier_pool" + "." + str(task_id) in name:
+                        param.requires_grad_(True)
+                    if "coef_k" + "." + str(task_id) in name:
+                        param.requires_grad_(True)
+                    if "coef_v" + "." + str(task_id) in name:
+                        param.requires_grad_(True)
     # Double check
     enabled = set()
     for name, param in model.named_parameters():
@@ -139,7 +141,7 @@ def run_class_incremental(cfg, device):
                 model(inputs, get_cur_feat=True)
 
     print(f"Parameters to be updated: {enabled}")
-    
+
     for task_id, _ in enumerate(eval_dataset):
 
         # negative_records = 0
